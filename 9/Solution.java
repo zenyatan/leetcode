1b15bf9618c6b6954953b27cc3092c9d55f1b788
NekrasovSergey <sergeyoprol@gmail.com>
2026-08-12 06:20:05 +0300

    feat: add the solution to problem 9

---
+++ 9/Solution.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        boolean isPal = true;
+        String strx = String.valueOf(x);
+        char[] chars = strx.toCharArray();
+        int end = chars.length;
+        int
+            left = 0,
+            right = end - 1;
+
+        if (x < 0) {
+            isPal = false;
+        }
+
+        for (int i = 0; i < (end / 2); i++) {
+            if (chars[left] == chars[right]) {
+                left++;
+                right--;
+            } else {
+                isPal = false;
+            }
+        }
+
+        return isPal;
+    }
+}
