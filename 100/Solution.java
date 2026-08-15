e8818e1ee7d8aa11c2a159752fff3436a8d66f29
NekrasovSergey <sergeyoprol@gmail.com>
2026-08-15 04:23:51 +0300

    fix: fix NullPointerException and runtime

--- 100/Solution.java
+++ 100/Solution.java
@@ -16,50 +16,18 @@
 
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        boolean isEq = true;
-        Integer 
-            amountNodes1 = p.getClass().getDeclaredFields().length,
-            amountNodes2 = q.getClass().getDeclaredFields().length,
-            root1 = p.val,
-            root2 = q.val,
-            left1 = 1,
-            right1 = 1,
-            left2 = 1,
-            right2 = 1;
         
+        if ((p == null) && (q == null)) {
+            return true;
+        } 
 
-        if (p.left == null) {
-            left1 = 0;
-        } else {
-            left1 = p.left.val;
+        if ((p == null) || (q == null)) {
+            return false;
         }
-
-        if (p.right == null) {
-            right1 = 0;
-        } else {
-            right1 = p.right.val;
+        
+        if ((p.val != q.val)) {
+            return false;
         }
-
-        if (q.left == null) {
-            left2 = 0;
-        } else {
-            left2 = q.left.val;
-        }
-
-        if (q.right == null) {
-            right2 = 0;
-        } else {
-            right2 = q.right.val;
-        }
-
-        if (amountNodes1 != amountNodes2) {
-            isEq = false;
-        } else if (root1 != root2) {
-            isEq = false;
-        } else if (!((left1 == left2) && (right1 == right2))) {
-            isEq = false;
-        }
-
-        return isEq;
+        return (isSameTree(p.left, q.left) && isSameTree(p.right, q.right));
     }
 }
